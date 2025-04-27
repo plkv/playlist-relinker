004699f08d8a87a7bf54027738beb7820b8e343b
Polyakov Stanislav <plkv@plkv-Macbook-Pro-14.local>
2025-04-27 16:51:06 +0200

    Switch to ClientCredentials flow

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
 from flask import Flask, render_template, request
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import SpotifyClientCredentials
+import os
 
 app = Flask(__name__)
 
-# 🔥 Твои данные
 CLIENT_ID = 'e727213173e141f482270557f6d11e26'
 CLIENT_SECRET = '924f0275c3214841a33331d0959e2c4f'
-REDIRECT_URI = 'https://localhost:8888/callback'
 
-# Авторизация для Spotify API
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
     client_id=CLIENT_ID,
-    client_secret=CLIENT_SECRET,
-    redirect_uri=REDIRECT_URI,
-    scope="playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private"
+    client_secret=CLIENT_SECRET
 ))
 
 @app.route('/', methods=['GET', 'POST'])
@@ -23,10 +19,7 @@ def index():
     if request.method == 'POST':
         playlist_url = request.form['playlist_url']
         try:
-            # Извлекаем ID плейлиста из URL
             playlist_id = playlist_url.split("/")[-1].split("?")[0]
-
-            # Получаем все треки из старого плейлиста
             results = sp.playlist_tracks(playlist_id)
             tracks = results['items']
 
@@ -46,15 +39,7 @@ def index():
                     else:
                         not_found_tracks.append(query)
 
-            # Создаем новый плейлист
-            user_id = sp.current_user()["id"]
-            new_playlist = sp.user_playlist_create(user=user_id, name="Offline to Online Playlist - Recovered", public=True, description="Converting old playlists based on offline mp3 files to online Spotify playlists")
-
-            # Добавляем найденные треки
-            if found_tracks:
-                sp.playlist_add_items(playlist_id=new_playlist['id'], items=found_tracks)
-
-            message = f"✅ Найдено и добавлено: {len(found_tracks)} треков. ❌ Не найдено: {len(not_found_tracks)} треков."
+            message = f"✅ Найдено: {len(found_tracks)} треков. ❌ Не найдено: {len(not_found_tracks)} треков."
 
         except Exception as e:
             message = f"Ошибка: {str(e)}"
@@ -62,4 +47,5 @@ def index():
     return render_template('index.html', message=message)
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
